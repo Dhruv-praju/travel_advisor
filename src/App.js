5ce7189084d28c299ed615632046f40a1aeaa8f5
dhruv <prajapati.dp@somaiya.edu>
2022-01-19 21:58:24 +0530

    rendered map

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from "react";
+import { CssBaseline, Grid } from "@mui/material";
+
+import Header from "./components/Header/Header";
+import List from './components/List/List'
+import Map from './components/Map/Map'
+import PlaceDetails from './components/PlaceDetails/PlaceDetails'
 
 const App = ()=>{
     return (
-        <h1>Hello world</h1>
+        <div>
+            <CssBaseline />
+            <Header />
+            <Grid container spacing={1}>
+                <Grid item xs={12} md={4}>
+                    <List />
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Map />
+                </Grid>
+            </Grid>
+        </div>
     )
 }
 
---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,37 @@
+import { Box } from "@mui/system";
+import React, {useState} from "react";
+import ReactMapGl from 'react-map-gl'
+
+const Map = ()=>{
+  const [viewPort, setViewPort] = useState({
+    latitude: 37.7577,
+    longitude: -122.4376,
+    width: "95%",
+    height: "100%",
+    zoom: 10
+  })
+  // current map window with co-ordinates is viewPort. As user drags of scrolls view Port that contaings current co-ordinates changes
+  // console.log(viewPort);
+
+    return (
+        <div>
+            <h1>Map</h1>
+            <Box sx={{
+              width:'100%',
+              height:850,
+            }}>
+                <ReactMapGl
+                  {...viewPort}
+                  mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+                  onViewportChange={viewport => setViewPort(viewport)}
+                  mapStyle='mapbox://styles/4everyhappy/ckylosdfn3lkx14l2dhot0rut'
+                >
+                  
+                </ReactMapGl>
+                
+            </Box>
+        </div>
+    )
+}
+
+export default Map
